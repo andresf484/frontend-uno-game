6a78bbfb8710cab73a06f535109f149cdffc5dc7
Andrés G <andres88884@hotmail.com>
2025-04-12 15:52:58 -0500

    Variable de entorno backend_url

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,3 +1,6 @@
+const backend_url = import.meta.env.VITE_BACKEND_URL;
+
 import { io } from 'socket.io-client';
 
-export const socket = io('http://localhost:3000');
+//export const socket = io('http://localhost:3000');
+export const socket = io(backend_url);
